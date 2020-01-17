5f8a9b98ffef42994ae319b51ee609f32fa1f70f
Delio <ddirazar@gmail.com>
2020-01-17 10:38:27 -0300

    Configuración de seguridad basic y firebase

--- instalacion/settings-template.php
+++ instalacion/settings-template.php
@@ -1,7 +1,9 @@
 <?php
 	return [
+			// Arreglo de configuración
 			'settings'=>[
-				'displayErrorDetails' =>true, 				
+
+				'displayErrorDetails' =>true, // En producción poner en FALSE				
 				'logger' =>[
 					'name' => 'backend',
 					'path' => '../instalacion/logs/web_services.log'
@@ -14,15 +16,20 @@
 						 "password" => "irsol",
 						 "charset"=>"WE8ISO8859P1"						
 				],
-				'authentication' =>[ 
-					"path"=>"/*",
-					"ignore"=>"/test",
-					"realm"=>"BackendRealm",
-					"users" => [
-							"usuario1" =>"CAMBIAR",
-						]						
-					
-				],
+				'authentication' =>["basic" =>[ 
+										"path"=>["/test","/api_docs","/*"],
+										"ignore"=>["/test"],
+										"realm"=>"BackendRealm",
+										"users" => [
+												"usuario1" =>"CAMBIAR",
+											]											
+										]  ,
+									"firebase" =>[ 
+										"path"=>"/servicios",
+										"projectId"=>"dinahuapi-intervan",
+										"keyCache"=>__DIR__.'/cache',
+										"timeoutCache"=>3600,					
+										] ],
 				"backend" => "erp"
-				]
+			]
 	];
